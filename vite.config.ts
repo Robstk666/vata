7dd883444bf5396d0668e59bd1d3f0efd61c6ab1
Rob_"G <robstk666@mail.ru>
2026-05-18 15:04:31 +0300

    Configure automatic GitHub Pages deployment using GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/ai_builder/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
